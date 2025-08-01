6b40e54d20788d09fe1f064b67cb64966ef86168
Elias Carabajal <EEliasCarabajal@gmail.com>
2025-08-01 03:28:27 -0300

    funcion para tomar valor del input

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-const pedirNombres = () =>{}
+let nombres = [];
+
+function agregarAmigo () {
+
+    let amigo = document.querySelector("#amigo").value; 
+   
+    return console.log (amigo)
+
+}
+
+
+
